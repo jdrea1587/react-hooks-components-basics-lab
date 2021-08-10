5e2858496724d476e09f97dd2ac0b6b19bf78f06
Josh <joshdanielrea@gmail.com>
2021-08-10 15:50:32 -0500

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,16 @@
 import React from "react";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
+import NavBar from "./NavBar"
+import Home from "./Home"
+import About from "./About"
 
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div id="about">
-      <h2>About</h2>
-    </div>
-  )
-}
 
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
-     <About /> 
+      <About /> 
     </div>
   );
 }
